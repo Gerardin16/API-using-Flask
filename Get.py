3f7e45368b8bca8ec9ec3ce6ba3b8d55968934c7
Gerardin <niveadha@gmail.com>
2021-01-13 23:30:40 +0530

    Get method implementation

---
+++ Get.py
@@ -0,0 +1,38 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# Create some family data in the form of a list of dictionaries.
+family = [
+    {'id': 0,
+     'name': 'Gerardin Niveadhana',
+     'age': '34',
+     'DOB': '16-09-1986'},
+    {'id': 1,
+     'name': 'Loyola Stalin',
+     'age': '40',
+     'DOB': '03-09-1980'},
+    {'id': 2,
+     'name': 'Jerwin Joseph',
+     'age': '6',
+     'DOB': '03-03-2015'},
+    {'id': 3,
+     'name': 'Jeffrin Stalin',
+     'age': '2',
+     'DOB': '30-01-2019'}
+]
+
+# A route to display Home page
+@app.route('/', methods=['GET'])
+def homepage():
+    return '<h1>Family Records</h1>'
+
+
+# A route to return all of the available entries in our family section.
+@app.route('/details/all', methods=['GET'])
+def get_details():
+    return jsonify(family)
+
+app.run()
